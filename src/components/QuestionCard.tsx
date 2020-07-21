179e72fa1645c9fe3bd61c31551cd3563e8f1c1b
Michaela Wagner <mickywagner@gmail.com>
2020-07-21 14:07:10 -0600

    Build out QuizCard

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,17 +2,37 @@ import React from 'react'
 
 type Props = {
     question: string;
-    answer: string[];
+    answers: string[];
     callback: any;
-    userAnswer: string;
+    userAnswer: boolean;
     questionNr: number;
     totalQuestion: number;
 }
 
-const QuestionCard = () => {
+const QuestionCard: React.FC<Props> = ({ 
+    question, 
+    answers,
+    userAnswer, 
+    callback, 
+    questionNr, 
+    totalQuestion
+}) => {
     return(
         <div>
-            Question Card
+           <p className="number">
+               Question: {questionNr} / {totalQuestion} 
+           </p>
+
+           <p dangerouslySetInnerHTML={{ __html: question }}></p>
+           <div>
+            {answers.map(answer => (
+                <div>
+                    <button disabled={userAnswer} onClick={callback}>
+                        <span dangerouslySetInnerHTML={{ __html: answer}} />
+                    </button>
+                </div>
+            ))}
+           </div>
         </div>
     )
 }
