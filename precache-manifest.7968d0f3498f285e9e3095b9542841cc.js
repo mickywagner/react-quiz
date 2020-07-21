4cf36ac64e44dbf965846a7c4b8d9db671f7e3eb
Michaela Wagner <mickywagner@gmail.com>
2020-07-21 17:27:09 -0600

    Updates

---
+++ precache-manifest.7968d0f3498f285e9e3095b9542841cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8d302a107abf1e7e67533bad493f28",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "7b9354b1799adcd4ca5d",
+    "url": "/react-quiz/static/js/2.f6d740e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-quiz/static/js/2.f6d740e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4abee05cacb962bf6aa",
+    "url": "/react-quiz/static/js/main.54a0f031.chunk.js"
+  },
+  {
+    "revision": "e8d353b2a9784522e501",
+    "url": "/react-quiz/static/js/runtime-main.c3646c95.js"
+  },
+  {
+    "revision": "e06288c36d403d64aa8a9c72d446bd30",
+    "url": "/react-quiz/static/media/dark_continent.e06288c3.jpg"
+  }
+]);
