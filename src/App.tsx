3115197ca1a6d24517d625794f346eeb25e098a5
Michaela Wagner <mickywagner@gmail.com>
2020-07-21 14:13:15 -0600

    Add props to question card in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React, { useState }from 'react';
 import QuizCard from './components/QuestionCard'
 import QuestionCard from './components/QuestionCard';
 
+const TOTAL_QUESTIONS = 10;
+
 const App = () => {
   const [loading, setLoading] = useState(false)
   const [questions, setQuestions] = useState([])
   const [number, setNumber] = useState(0)
-  const [userAnswer, setUserAnswer] = useState([])
+  const [userAnswers, setUserAnswers] = useState([])
   const [score, setScore] = useState(0)
   const [gameOver, setGameOver] = useState(true)
-  
+
   const startTrivia = async () => {
 
   }
@@ -28,7 +30,15 @@ const App = () => {
       <button className="start" onClick={startTrivia}>Start</button>
       <p className="score">Score:</p>
       <p>Loading Questions...</p>
-      <QuestionCard />
+      <QuestionCard 
+        questionNr={number + 1}
+        totalQuestion={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+
+      />
       <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
