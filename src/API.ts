96821d12bb4fa127a9dc675dcb223ca06ed719f9
Michaela Wagner <mickywagner@gmail.com>
2020-07-21 14:40:15 -0600

    fix typos add types to App

--- src/API.ts
+++ src/API.ts
@@ -1,9 +1,10 @@
+import { shuffleArray } from './utils'
 
 export type Question = {
     category: string;
     correct_answer: string;
     difficulty: string;
-    incorrect_answer: string[];
+    incorrect_answers: string[];
     question: string;
     type: string;
 }
@@ -23,7 +24,9 @@ export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty)
     return data.results.map((question: Question) => (
         {
             ...question, 
-            answer:
+            answers: shuffleArray([
+                ...question.incorrect_answers, 
+                question.correct_answer])
         }
     ))
 }
--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import QuizCard from './components/QuestionCard'
 import QuestionCard from './components/QuestionCard';
 
 import { fetchQuizQuestions } from './API';
-import { Difficulty } from './API';
+import { QuestionState, Difficulty } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [loading, setLoading] = useState(false)
-  const [questions, setQuestions] = useState([])
+  const [questions, setQuestions] = useState<QuestionState[]>([])
   const [number, setNumber] = useState(0)
   const [userAnswers, setUserAnswers] = useState([])
   const [score, setScore] = useState(0)
